77ffc881f1eabdc843318f307a4f808c6aa2f7cf
NarendraGore <ngore4981@gmail.com>
2026-04-02 17:38:47 +0530

    Order DTO Created

---
+++ SmartInventoryAPI/DTOs/Order/CreateOrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartInventoryAPI.DTOs.Order
+{
+    public class CreateOrderDto
+    {
+        public string CustomerName { get; set; } 
+        public List<OrderItemDto> Items { get; set; }
+    }
+}
