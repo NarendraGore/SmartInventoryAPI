9633b1e16953a0e5dcbba5f0ded8957e00b444d1
NarendraGore <ngore4981@gmail.com>
2026-04-03 10:38:59 +0530

    Bulk Price Update DTO

---
+++ SmartInventoryAPI/DTOs/Product/BulkPriceUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartInventoryAPI.DTOs.Product
+{
+    public class BulkPriceUpdateDto
+    {
+        public int ProductId { get; set; }
+        public decimal NewPrice { get; set; }
+    }
+}
